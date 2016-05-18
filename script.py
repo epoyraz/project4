7573b006d3ef7e703c31035e5b959ebf3e598ea3
epoyraz <epoyraz.eth@googlemail.com>
2016-05-18 09:28:58 +0200

    initial commit

---
+++ script.py
@@ -0,0 +1,44 @@
+
+
+
+w, h = 19, 19
+Matrix = [[0 for x in range(w)] for y in range(h)]
+
+Matrix[0][0] = -0.724
+Matrix[0][1] = -0.903
+Matrix[0][2] = -0.722
+Matrix[0][3] = -0.322
+
+print Matrix
+
+
+
+
+
+from Bio.PDB.PDBParser import PDBParser
+
+parser = PDBParser()
+
+structure = parser.get_structure('155l', '155l.pdb')
+header = parser.get_header()
+trailer = parser.get_trailer()
+
+
+# remove hydrogen according to  : http://pelican.rsvs.ulaval.ca/mediawiki/index.php/Manipulating_PDB_files_using_BioPython
+for model in structure:
+    for chain in model:
+        for residue in chain:
+            id = residue.id
+            if id[0] == 'W':
+               chain.detach_child(id)
+        if len(chain) == 0:
+            model.detach_child(chain.id)
+
+
+
+for model in structure:
+   for chain in model:
+      for residue in chain:
+         for residue2 in chain:
+            distance = residue['CA'] - residue2['CA']
+            print "distance is :" + distance
